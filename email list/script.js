614df3d684a4f85724f809d8112493971e734f25
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-15 16:47:10 +0100

    mail list and print text

--- email list/script.js
+++ email list/script.js
@@ -2,3 +2,18 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere
 // (createvi una piccola lista di email),
 //stampa un messaggio appropriato sull’esito del controllo.
+
+const mailList = [ "ale.27@gmail.com","mario.rossi34@gmail.com", "marco.marchesi22@libero.it", "marta.43@virgilio.it", "gianna.giannetti@gmail.com",
+                    "franca99@libero.it", "paperella_pazza@paperella.com"
+                ]
+const mailName = (prompt("inserisci la tua mail"));
+
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//fruits.includes("Banana", 3);
+
+if (mailList.includes(mailName)) {
+    console.log("La tua mail è valida");
+}
+else {
+    console.log("La tua mail non è valida");
+}
